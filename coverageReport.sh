2908bbdcdbbd11b4177036f6b5d28818a39e66d7
shubhadip <shubhadip.maity@gmail.com>
2019-05-07 12:13:51 +0530

    decreasing code coverage

--- coverageReport.sh
+++ coverageReport.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
 
-TEST_DIRECTORY='/'
-TEMPDIR="$PWD$TEST_DIRECTORY"
-cd $TEMPDIR
-yarn
-npm run test:unit
+# TEST_DIRECTORY='/'
+# TEMPDIR="$PWD$TEST_DIRECTORY"
+# cd $TEMPDIR
+# yarn
+# npm run test:unit
+
+# exec bash 'bash <(curl -s https://codecov.io/bash) -t 7d998c9d-a489-4246-ad85-d94a2abfccee'
+# echo $DATA
+
+# curl -s https://codecov.io/bash -t 7d998c9d-a489-4246-ad85-d94a2abfccee
+
+CT="7d998c9d-a489-4246-ad85-d94a2abfccee"
+
+# TEST="curl -s https://codecov.io/bash -t $CT"
+# echo $TEST
+
+# RESPONSE=`$TEST`
+# echo $RESPONSE
+
+curlCmd="curl -s 'https://codecov.io/bash' -t $CT > ~/fb.txt"
+eval $curlCmd
